b8b0075f6a89d7878492e0e6510eaf00a233bed4
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-12-04 19:11:13 +0200

    facebook auth implementation

--- lib/Features/Auth/data/repos/auth_repo_impl.dart
+++ lib/Features/Auth/data/repos/auth_repo_impl.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:fruit_ecommerce_app/Features/Auth/domain/repos/auth_repo.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -10,8 +11,13 @@ class AuthRepoImpl extends AuthRepo {
   }
 
   @override
-  Future loginWithFacebook() {
-    throw UnimplementedError();
+  Future loginWithFacebook() async {
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    final OAuthCredential facebookAuthCredential =
+        FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
   }
 
   @override
