2f35eef3371796e2349c0c4c3c225f857482e5bc
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-12-04 19:56:35 +0200

    add either to auth implementation and auth repo

--- lib/Features/Auth/data/repos/auth_repo_impl.dart
+++ lib/Features/Auth/data/repos/auth_repo_impl.dart
@@ -1,3 +1,4 @@
+import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:fruit_ecommerce_app/Features/Auth/domain/repos/auth_repo.dart';
@@ -11,27 +12,32 @@ class AuthRepoImpl extends AuthRepo {
   }
 
   @override
-  Future loginWithFacebook() async {
-    final LoginResult loginResult = await FacebookAuth.instance.login();
+  Future<UserCredential> loginWithFacebook() async {
 
-    final OAuthCredential facebookAuthCredential =
-        FacebookAuthProvider.credential(loginResult.accessToken!.token);
+      final LoginResult loginResult = await FacebookAuth.instance.login();
+
+      final OAuthCredential facebookAuthCredential =
+          FacebookAuthProvider.credential(loginResult.accessToken!.token);
 
     return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
   }
 
   @override
-  Future loginWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  Future<Either<Exception, UserCredential>> loginWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-    final GoogleSignInAuthentication? googleAuth =
-        await googleUser?.authentication;
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
 
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
-    );
-
-    return await FirebaseAuth.instance.signInWithCredential(credential);
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+      return right(await FirebaseAuth.instance.signInWithCredential(credential));
+      // return await FirebaseAuth.instance.signInWithCredential(credential);
+    } catch (e) {
+      return left(Exception('something went wrong'));
+    }
   }
 }
